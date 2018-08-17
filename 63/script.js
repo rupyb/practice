a81231cf46a30b8e2baec88dd28508137cd32bbd
rupyb <rupybrisman@yahoo.com>
2018-08-17 10:54:27 -0400

    first commit

---
+++ 63/script.js
@@ -0,0 +1,32 @@
+(function () {
+    'use strict';
+
+    $('form').submit((event) => {
+        const textArea = $('#theTextArea');
+        // const theInput = $('#inputFile');
+        // console.log('theInput', theInput);
+        const file = $('#inputFile').val();
+        const spinner = $('#loader');
+        spinner.hide();
+        //  console.log('file ', file);
+        //  console.log('file.value ', file.value);
+        $.get(file, (loadedData) => {
+            spinner.show();
+            setTimeout(() => {
+                textArea.text(loadedData);
+                spinner.hide();
+            }, 3000);
+
+            console.log(loadedData);
+        })
+            .fail((xhr, statusCode, statusText) => {
+                textArea.text(`That File Is unavailable ${statusText} ${statusCode}`);
+                console.log(xhr);
+                console.log(statusCode);
+                console.log(statusText);
+            });
+
+        console.log(file);
+        event.preventDefault();
+    });
+}());
